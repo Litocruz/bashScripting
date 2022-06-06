5365dccb467c7541fee9c76eb4d190cf6955f651
Litocruz <rebgolamadrid@hotmail.com>
2022-06-05 21:16:29 -0300

    parametros

---
+++ 4_argumentos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# script para identificar variables
+
+nombreCurso=$1
+horarioCurso=$2
+
+echo "El curso $nombreCurso dictado en el horario $horarioCurso"
+echo "El numero de parametros enviados $#"
+echo "Los parametros son $*"
+
