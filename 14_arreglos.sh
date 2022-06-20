7156967509a3dd63a47c6a57df03681c2e7dc21f
Litocruz <rebgolamadrid@hotmail.com>
2022-06-20 17:35:05 -0300

    Terminado script de arreglos

--- 14_arreglos.sh
+++ 14_arreglos.sh
@@ -17,11 +17,16 @@ echo "Tamaño Arreglo Cadenas: ${#arregloCadenas[*]}"
 echo "Tamaño Arreglo Rangos: ${#arregloRangos[*]}"
 
 echo "Imprimir posicion 3 de los arreglos "
-echo "Tamaño Arreglo Numeros: ${arregloNumeros[3]}"
-echo "Tamaño Arreglo Cadenas: ${arregloCadenas[3]}"
-echo "Tamaño Arreglo Rangos: ${arregloRangos[3]}"
+echo "Posicion 3 Arreglo Numeros: ${arregloNumeros[3]}"
+echo "Posicion 3 Arreglo Cadenas: ${arregloCadenas[3]}"
+echo "Posicion 3 Arreglo Rangos: ${arregloRangos[3]}"
 
-echo "Añadir y Eliminar Valores en un arreglo"
-echo "Tamaño Arreglo Numeros: ${arregloNumeros[3]}"
-echo "Tamaño Arreglo Cadenas: ${arregloCadenas[3]}"
-echo "Tamaño Arreglo Rangos: ${arregloRangos[3]}"
+echo "Añadir y Eliminar Valores en arreglo numeros"
+arregloNumeros[7]=20
+unset arregloNumeros[0]
+echo "Arreglo Numeros: ${arregloNumeros[*]}"
+echo "Tamaño Arreglo Numeros: ${#arregloNumeros[*]}"
+echo "Posicion 0 Arreglo Numeros: ${arregloNumeros[0]}"
+echo "Posicion 1 Arreglo Numeros: ${arregloNumeros[1]}"
+echo "Posicion 5 Arreglo Numeros: ${arregloNumeros[5]}"
+echo "Posicion 6 Arreglo Numeros: ${arregloNumeros[6]}"
